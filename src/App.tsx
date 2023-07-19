97605d647a9a9e86addecc06a27107fa2562d7fd
saarijk <>
2023-07-19 14:36:03 +0300

    feat: displays selected recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Home from "./components/Home"
 import Nav from "./components/Nav"
+
 function App() {
-  return <>
-  <Nav name={"Home"} />
-  <div className="app bg-gray-20">
-    <h1 className=" text-primary-500 text-center text-3xl pt-20">Recipe Explorer</h1>
-    <br/>
-    <p className="text-center text-md mt-[-20px] mb-10">Insert description here.</p>
-    <Home/>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Nav name={"Home"} />
+      <div className="flex-grow bg-gray-20">
+        <h1 className="text-primary-500 text-center text-3xl pt-20">Recipe Explorer</h1>
+        <br/>
+        <p className="text-center text-md mt-[-20px] mb-10">Insert description here.</p>
+        <Home />
+      </div>
     </div>
-  </>;
+  );
 }
+
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { Meal, SearchResult, idkMeal } from "@/types/Recipe"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
@@ -9,11 +9,21 @@ export default function Home() {
   const [allMeals, setAllMeals] = useState<idkMeal>({meals: []});
   let [searchResults, setSearchResults] = useState<SearchResult[]>([]);
   const [check, setCheck] = useState<string | null>(null);
+  const [selectedRecipe, setSelectedRecipe] = useState<Meal | null>(null);
+  const selectedRecipeRef = useRef<HTMLDivElement>(null);
 
   function findRecipes(elementToFind: string) {
     const updatedSearchResults: SearchResult[] = [];
+    let foundName = false;
   
     for (let i = 0; i < allMeals?.meals.length; i++) {
+      foundName = searchWithinName(elementToFind, i);
+      if (foundName) {
+        updatedSearchResults.push({
+          name: allMeals.meals[i].strMeal,
+          image: allMeals.meals[i].strMealThumb,
+        })
+      }
       if (allMeals?.meals[i].strCategory.toLowerCase() === elementToFind.toLowerCase()) {
         // Add to searchResults array
         updatedSearchResults.push({
@@ -32,11 +42,39 @@ export default function Home() {
           });
           break; // Exit the loop since the ingredient is found
         }
-      }
+      } 
+    }
+
+    function searchWithinName(nameToFind:string, index:number) :boolean {
+        const stringInArray = allMeals.meals[index];
+        const wordsInString = stringInArray.split(" ");
+    
+        for (let j = 0; j < wordsInString.length; j++) {
+          if (wordsInString[j].toLowerCase() === nameToFind.toLowerCase()) {
+            return true; // Word found in the array
+          }
+        }
+      return false;
     }
   
     setSearchResults(updatedSearchResults);
     console.log(searchResults);
+  }
+
+  async function showRecipe(mealName: string) {
+    try {
+      const encodedName = encodeURIComponent(mealName);
+      const fullRecipeUrl = searchUrl + encodedName;
+      const response = await axios.get<idkMeal>(fullRecipeUrl);
+  
+      if (response.data && response.data.meals.length > 0) {
+        setSelectedRecipe(response.data.meals[0]);
+      } else {
+        console.error("Recipe not found");
+      }
+    } catch (error) {
+      console.error("Error fetching the recipe:", error);
+    }
   }
 
   useEffect(() => {
@@ -52,6 +90,13 @@ export default function Home() {
     }; 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    // Scroll to the selectedRecipe section when it is updated
+    if (selectedRecipeRef.current) {
+      selectedRecipeRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [selectedRecipe]);
 
     return (
       <>
@@ -73,6 +118,51 @@ export default function Home() {
           >Search</button>
           <p>You searched for:</p>
           {check !== null && check !== undefined && <p>{check} with {searchResults.length} results!</p>}
+        </div>
+        {selectedRecipe ? (
+          <div ref={selectedRecipeRef} className="flex flex-col items-center">
+            <h1 className="text-3xl text-center text-primary-500 mb-3 mt-[100px]">{selectedRecipe.strMeal}</h1>
+            <img className="block mx-auto mb-3" src={selectedRecipe.strMealThumb}/>
+            <h2 className="text-gray-500 text-center text-xl mb-3">Ingredients</h2>
+            <p className="text-center">{selectedRecipe.strMeasure1} {selectedRecipe.strIngredient1}</p>
+            <p className="text-center">{selectedRecipe.strMeasure2} {selectedRecipe.strIngredient2}</p>
+            <p className="text-center">{selectedRecipe.strMeasure3} {selectedRecipe.strIngredient3}</p>
+            <p className="text-center">{selectedRecipe.strMeasure4} {selectedRecipe.strIngredient4}</p>
+            <p className="text-center">{selectedRecipe.strMeasure5} {selectedRecipe.strIngredient5}</p>
+            <p className="text-center">{selectedRecipe.strMeasure6} {selectedRecipe.strIngredient6}</p>
+            <p className="text-center">{selectedRecipe.strMeasure7} {selectedRecipe.strIngredient7}</p>
+            <p className="text-center">{selectedRecipe.strMeasure8} {selectedRecipe.strIngredient8}</p>
+            <p className="text-center">{selectedRecipe.strMeasure9} {selectedRecipe.strIngredient9}</p>
+            <p className="text-center">{selectedRecipe.strMeasure10} {selectedRecipe.strIngredient10}</p>
+            <p className="text-center">{selectedRecipe.strMeasure11} {selectedRecipe.strIngredient11}</p>
+            <p className="text-center">{selectedRecipe.strMeasure12} {selectedRecipe.strIngredient12}</p>
+            <p className="text-center">{selectedRecipe.strMeasure13} {selectedRecipe.strIngredient13}</p>
+            <p className="text-center">{selectedRecipe.strMeasure14} {selectedRecipe.strIngredient14}</p>
+            <p className="text-center">{selectedRecipe.strMeasure15} {selectedRecipe.strIngredient15}</p>
+            <p className="text-center">{selectedRecipe.strMeasure16} {selectedRecipe.strIngredient16}</p>
+            <p className="text-center">{selectedRecipe.strMeasure17} {selectedRecipe.strIngredient17}</p>
+            <p className="text-center">{selectedRecipe.strMeasure18} {selectedRecipe.strIngredient18}</p>
+            <p className="text-center">{selectedRecipe.strMeasure19} {selectedRecipe.strIngredient19}</p>
+            <p className="text-center">{selectedRecipe.strMeasure20} {selectedRecipe.strIngredient20}</p>
+            <h2 className="text-gray-500 text-center text-xl mb-3 mt-3">Instructions</h2>
+            <p className="w-3/4 text-justify flex-grow">{selectedRecipe.strInstructions}</p>
+          </div>
+        ) : null}
+        <h1 className="text-primary-500 text-center text-3xl pt-20 mb-10">All Recipes</h1>
+        <div className="flex justify-center"> 
+          <div className="recipe-list bg-gray-20 w-5/6 flex flex-wrap justify-center gap-8"> 
+            {allMeals.meals.map((meal, index) => (
+              <div key={index} className="recipe-card">
+                <img src={meal.strMealThumb} alt={meal.name} className="recipe-image" />
+                <h2
+                  className="w-[150px] text-center hover:text-primary-500 hover:cursor-pointer"
+                  onClick={() => showRecipe(meal.strMeal)}
+                >
+                  {meal.strMeal}
+                </h2>
+              </div>
+            ))}
+          </div>
         </div>
       </>
     )
